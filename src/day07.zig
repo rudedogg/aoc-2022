4849a1de37e498b6392d4fc69c6c21bfe4387281
Austin Rude <rudedogg@users.noreply.github.com>
2022-12-07 15:44:36 -0700

    Clean up and split into functions

--- src/day07.zig
+++ src/day07.zig
@@ -8,19 +8,22 @@ const BitSet = std.DynamicBitSet;
 const util = @import("util.zig");
 const gpa = util.gpa;
 
-const data = @embedFile("data/day07.txt");
-
-const Directory = struct {
-    name: []const u8,
-    size: usize,
-};
-
 pub fn main() !void {
-    const file_tree = try parseInput(data);
-    _ = file_tree;
+    const data = @embedFile("data/day07.txt");
+    const part1_total_size = try solvePart1(data);
+    std.debug.print("Part 1 Total: {d}\n", .{part1_total_size});
 }
 
-fn parseInput(input: []const u8) !usize {
+fn solvePart1(input: []const u8) !usize {
+    const directory_size_map = try parseInput(input);
+    const total_map = try accumulateDirectorySizes(directory_size_map);
+
+    const total_size = sumValuesLessThan(total_map, 100_000);
+    return total_size;
+}
+
+/// Parse the input into a StringHashMap with values of usize (representing total size of file(s) directly in that dir)
+fn parseInput(input: []const u8) !std.StringHashMap(usize) {
     var path = List(u8).init(gpa);
     var map = std.StringHashMap(usize).init(gpa);
 
@@ -47,6 +50,7 @@ fn parseInput(input: []const u8) !usize {
                 std.debug.print("Path: {s}\n", .{path.items});
             }
         } else if (std.mem.eql(u8, line[0..4], "$ ls")) {
+            std.debug.print("ls Path: {s}\n", .{path.items});
             continue;
         } else if (line[0] >= '0' and line[0] <= '9') {
             var file_iterator = std.mem.split(u8, line, " ");
@@ -67,29 +71,51 @@ fn parseInput(input: []const u8) !usize {
             continue;
         }
     }
+
+    return map;
+}
+
+/// Iterate over the paths, adding any child paths totals to their total, and return it as a new StringHashMap(usize)
+fn accumulateDirectorySizes(original_map: std.StringHashMap(usize)) !std.StringHashMap(usize) {
+    var total_map = std.StringHashMap(usize).init(gpa);
+    var original_map_iterator = original_map.iterator();
+    while (original_map_iterator.next()) |entry| {
+        try total_map.putNoClobber(entry.key_ptr.*, entry.value_ptr.*);
+
+        var nested_map_iterator = original_map.iterator();
+        while (nested_map_iterator.next()) |nested_entry| {
+            var existing_value = total_map.get(entry.key_ptr.*);
+            if (std.mem.startsWith(u8, nested_entry.key_ptr.*, entry.key_ptr.*) and std.mem.eql(u8, nested_entry.key_ptr.*, entry.key_ptr.*) == false) {
+                try total_map.put(entry.key_ptr.*, existing_value.? + nested_entry.value_ptr.*);
+            }
+        }
+    }
+
+    return total_map;
+}
+
+fn sumValuesLessThan(map: std.StringHashMap(usize), max_size: usize) usize {
     var total: usize = 0;
     var value_iterator = map.valueIterator();
     while (value_iterator.next()) |value| {
-        if (value.* < 100000) {
+        if (value.* < max_size) {
             total = total + value.*;
         }
     }
-    var map_iterator = map.iterator();
-    while (map_iterator.next()) |entry| {
-        std.debug.print("Path: {s}\n  = {d}\n\n", .{ entry.key_ptr.*, entry.value_ptr.* });
-    }
-    std.debug.print("total: {d}\n", .{total});
+
     return total;
-    // return map;
-    // 1381590 too low
-    // 1636474 too high
 }
 
-test "input 1" {
+test "Sample" {
     const test_data = @embedFile("data/day07test.txt");
-    const total = try parseInput(test_data);
+    const directory_size_map = try parseInput(test_data);
+    const total_map = try accumulateDirectorySizes(directory_size_map);
 
-    try std.testing.expect(total == 48381165);
+    const total_size = sumValuesLessThan(total_map, 100_000);
+
+    std.log.warn("total: = {d}\n\n", .{total_size});
+
+    try std.testing.expect(total_size == 95437);
 }
 
 // Useful stdlib functions
